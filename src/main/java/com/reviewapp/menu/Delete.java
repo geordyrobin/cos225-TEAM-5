a943b765310432ef918ec4fdded28723ca8d02d4
HaffordD <drew.hafford@maine.edu>
2024-11-15 13:48:34 -0500

    Added the method to remove the specified review
    Accidentally forgot to commit for the majority of this process. It's just a modified version of the one found on mongodb

--- src/main/java/com/reviewapp/menu/Delete.java
+++ src/main/java/com/reviewapp/menu/Delete.java
@@ -1,25 +1,31 @@
-package src.main.java.com.reviewapp.menu;
-// import java.util.Scanner;
+package com.reviewapp.menu;
+import java.util.Scanner;
 import org.bson.Document;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.FindOneAndUpdateOptions;
-import com.mongodb.client.model.ReturnDocument;
-import com.mongodb.client.model.UpdateOptions;
-import com.mongodb.client.result.UpdateResult;
+import com.mongodb.client.result.DeleteResult;
 import org.bson.conversions.Bson;
-import org.bson.json.JsonWriterSettings;
-import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
-import static com.mongodb.client.model.Updates.*;
 
 public class Delete{
     public static void main(String[] args) {
-            System.out.println("Hello, Which review would you like to delete");
+            System.out.println("Hello, Which review would you like to delete\"");
+            Scanner choice = new Scanner(System.in);  // Create a Scanner object
+            String target = choice.nextLine();
+            try (MongoClient mongoClient = MongoClients.create(System.getProperty("mongodb.uri"))) {
+            MongoDatabase uberReviewDB = mongoClient.getDatabase("uberReviewData");
+            MongoCollection<Document> gradesCollection = uberReviewDB.getCollection("reviews");
+            // delete one document
+            Bson filter = eq("id", target);
+            DeleteResult result = gradesCollection.deleteOne(filter);
+            System.out.println(result);
+            //take the input, either looking for id or the entire line
 
+            //using the update function found on mongodb, either use remove to nuke the entire line or delete by parts
   
   }
 
+}
 }
