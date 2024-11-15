8ed80b5be75c376ce1a958f7459fbc06e44c0193
HaffordD <drew.hafford@maine.edu>
2024-11-15 12:34:01 -0500

    Fixed delete and adjusted pom.xml

---
+++ src/main/java/com/reviewapp/menu/Delete.java
@@ -0,0 +1,25 @@
+package src.main.java.com.reviewapp.menu;
+// import java.util.Scanner;
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.FindOneAndUpdateOptions;
+import com.mongodb.client.model.ReturnDocument;
+import com.mongodb.client.model.UpdateOptions;
+import com.mongodb.client.result.UpdateResult;
+import org.bson.conversions.Bson;
+import org.bson.json.JsonWriterSettings;
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Updates.*;
+
+public class Delete{
+    public static void main(String[] args) {
+            System.out.println("Hello, Which review would you like to delete");
+
+  
+  }
+
+}
