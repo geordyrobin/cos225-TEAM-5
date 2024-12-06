4581df4294e692ca70e312bcb187ed6483d8aa45
ZoCarroll <zo.carroll@maine.edu>
2024-12-06 11:03:26 -0500

    normalizing formating

---
+++ Feedback.md
@@ -0,0 +1,8 @@
+# Feedback Group 5
+
+## Overall Feedback for Deliverable #2
+
+I would recommmend creating a folder to put your Java files (`src` folder) and the `pom.xml` file in an app folder. While keeping the README file outside. Missing dataset and necessary files to parse the dataset and create entries on the database.
+
+Points - 30/100
+
---
+++ src/main/java/com/reviewapp/menu/Delete.java
@@ -0,0 +1,28 @@
+package com.reviewapp.menu;
+import java.util.Scanner;
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.DeleteResult;
+import org.bson.conversions.Bson;
+import static com.mongodb.client.model.Filters.eq;
+
+abstract public class Delete{
+    public static void main(String[] args) {
+            System.out.println("Hello, Which review would you like to delete\"");
+            Scanner choice = new Scanner(System.in);  // Create a Scanner object
+            String target = choice.nextLine();
+            try (MongoClient mongoClient = MongoClients.create(System.getProperty("mongodb.uri"))) {
+            MongoDatabase uberReviewDB = mongoClient.getDatabase("uberReviewData");
+            MongoCollection<Document> gradesCollection = uberReviewDB.getCollection("reviews");
+            // delete one document
+            Bson filter = eq("id", target);
+            DeleteResult result = gradesCollection.deleteOne(filter);
+            System.out.println(result);
+  
+  }
+
+}
+}
